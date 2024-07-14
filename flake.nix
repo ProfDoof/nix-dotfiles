494987305cb9540f9391966ed59c3c67557dc5c5
John Marsden <john@johnmarsden.dev>
2024-07-14 11:59:05 -0400

    Add nixpkgs-wayland overlay

--- flake.nix
+++ flake.nix
@@ -1,6 +1,6 @@
 {
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixos-cosmic = {
       url = "github:lilyinstarlight/nixos-cosmic";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -9,38 +9,42 @@
       url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
+    nixpkgs-wayland = {
+      url = "github:nix-community/nixpkgs-wayland";
+    };
   };
-  
-  outputs = { self, nixpkgs, nixos-cosmic, home-manager }: 
+
+  outputs = { self, nixpkgs, nixos-cosmic, home-manager, nixpkgs-wayland }:
     let
       inherit (nixpkgs) lib;
+      system = "x86_64-linux";
+      modules = [
+        nixos-cosmic.nixosModules.default
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+          home-manager.users.john = import ./users/john/home.nix;
+        }
+        ({ pkgs, config, ... }: {
+          config.nixpkgs.overlays = [ nixpkgs-wayland.overlay ];
+        })
+      ];
+      hosts = (
+        lib.filterAttrs
+          (host: type: type == "directory" && builtins.pathExists (./hosts + "/${host}/configuration.nix"))
+          (builtins.readDir ./hosts)
+      );
     in
     {
-      nixosConfigurations = 
-        let
-          modules = [
-            nixos-cosmic.nixosModules.default
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.users.john = import ./users/john/home.nix;
-            }
-          ];
-          hosts = (
-            lib.filterAttrs
-              (host: type: type == "directory" && builtins.pathExists (./hosts + "/${host}/configuration.nix"))
-              (builtins.readDir ./hosts)
-          );
-        in
+      nixosConfigurations =
         lib.mapAttrs
           (host: _: lib.nixosSystem {
-            system = "x86_64-linux";
+            inherit system;
             modules = modules ++ [
               ./hosts/${host}/configuration.nix
             ];
           })
           hosts;
-      
     };
 }
