a4b8ba61a26f313b06afc74932d95cfa9e420e9e
John Marsden <john@johnmarsden.dev>
2024-09-15 15:56:27 -0400

    Enable nix-darwin fish and zsh program to access darwin-rebuild hopefully

--- hosts/darwin/isabella/configuration.nix
+++ hosts/darwin/isabella/configuration.nix
@@ -1,8 +1,15 @@
 { pkgs, ... }:
 {
+    environment.systemPackages = with pkgs; [
+
+    ];
+
     # Auto upgrade nix package and the daemon service.
+    programs.zsh.enable = true;
+    programs.fish.enable = true;
     services.nix-daemon.enable = true;
     nix.package = pkgs.nix;
     nixpkgs.hostPlatform = "x86_64-darwin";
+    system.configurationRevision = self.rev or self.dirtyRev or null;
     system.stateVersion = 5;
 }
