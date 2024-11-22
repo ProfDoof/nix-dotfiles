e9bda77ba12acb996976abced7aa17a39c505a0d
John Marsden <john@johnmarsden.dev>
2024-11-21 22:39:36 -0500

    Switch lib to builtins

--- users/john/ergo/talon.nix
+++ users/john/ergo/talon.nix
@@ -1,9 +1,9 @@
-{ lib, ... }:
+{ ... }:
 
 {
   home.file = {
     ".talon/user/community" = {
-      source = lib.fetchFromGithub {
+      source = builtins.fetchFromGithub {
         owner = "talonhub";
         repo = "community";
         rev = "bb240ba1cf958bd3e2c7791e5a57d09d205adc81";
@@ -12,7 +12,7 @@
       recursive = true;
     };
     ".talon/user/rango" = {
-      source = lib.fetchFromGithub {
+      source = builtins.fetchFromGithub {
         owner = "david-tejada";
         repo = "rango-talon";
         rev = "609641f8fe99fafe7e6f69441f60792fb7c51b60";
@@ -21,7 +21,7 @@
       recursive = true;
     };
     ".talon/user/cursorless" = {
-      source = lib.fetchFromGithub {
+      source = builtins.fetchFromGithub {
         owner = "cursorless-dev";
         repo = "cursorless-talon";
         rev = "0aba9e01299637655c7ca11cba7c99b965a1a488";
@@ -30,7 +30,7 @@
       recursive = true;
     };
     ".talon/user/talon_hud" = {
-      source = lib.fetchFromGithub {
+      source = builtins.fetchFromGithub {
         owner = "chaosparrot";
         repo = "talon_hud";
         rev = "0d676565b95f34841d7268b3667b4b780e50cfaa";
