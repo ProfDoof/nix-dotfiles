82839750c448dbb43bf8a637036624dbaf193e40
John Marsden <john@johnmarsden.dev>
2024-11-24 16:13:52 -0500

    Set up loading previously loaded game settings

--- users/john/ergo/gamemode/gamemode.py
+++ users/john/ergo/gamemode/gamemode.py
@@ -1,5 +1,11 @@
 from talon import Module, actions, Context, settings
 from pprint import pprint
+from collections import defaultdict
+from enum import Enum, auto
+
+class GameSetting(Enum):
+    ActiveStickyKeys = auto()
+    StickyKeys = auto()
 
 mod = Module()
 mod.mode("gamemode", desc="A mode to game")
@@ -8,18 +14,22 @@ mod.setting("active_game_name", type=str, default="",
     desc="Set the active game's name for use in loading settings from previous play "
          "(currently only works during continuous sessions of talon)")
 
+
 gamemode_ctx = Context()
 gamemode_ctx.matches = '''
 mode: user.gamemode
 tag: user.in_game
 '''
-game_settings: dict[str, set[str]] = {}
-active_sticky_keys: set[str] = set()
-sticky_keys: set[str] = set()
+
+game_settings: dict[str, dict[GameSetting, set[str]]] = defaultdict(lambda: defaultdict(set))
+current_game_settings: dict[GameSetting, set[str]] = defaultdict(set)
 
 def load_active_game_data(*args):
-    print("Active game changed")
-    pprint(args)
+    (game_name,) = args
+
+    global current_game_settings
+    current_game_settings = game_settings[game_name]
+    
 
 settings.register("user.active_game_name", load_active_game_data)
 
@@ -42,6 +52,7 @@ class InGameGameModeActions:
 
     def toggle(s: str) -> None:
         "Toggle a key from up to down or down to up"
+        active_sticky_keys = current_game_settings[GameSetting.ActiveStickyKeys]
         if s in active_sticky_keys:
             actions.key(f'{s}:up')
             active_sticky_keys.remove(s)
@@ -51,9 +62,9 @@ class InGameGameModeActions:
 
     def sticky(s: str) -> None:
         "Add a key to the sticky keys"
-        sticky_keys.add(s)
+        current_game_settings[GameSetting.StickyKeys].add(s)
 
     def unsticky(s: str) -> None:
         "Remove a key from the sticky keys"
-        if s in sticky_keys:
-            sticky_keys.remove(s)
+        if s in current_game_settings[GameSetting.StickyKeys]:
+            current_game_settings[GameSetting.StickyKeys].remove(s)
