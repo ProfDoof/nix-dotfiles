c6a02363d57c45aabb3855ca55c12a354f372e36
John Marsden <john@johnmarsden.dev>
2024-12-29 12:11:38 -0500

    Add talonfmt

--- flake.nix
+++ flake.nix
@@ -190,6 +190,7 @@
             nil
             bashInteractive
             python3
+            pipx
           ];
         };
 
--- treefmt.nix
+++ treefmt.nix
@@ -1,8 +1,17 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   projectRootFile = "flake.nix";
   programs.nixfmt.enable = true;
+  settings.formatter."talonfmt" = {
+    command = "${pkgs.pipx}/bin/pipx";
+    options = [
+      "run"
+      "talonfmt"
+      "--in-place"
+    ];
+    includes = [ "*.talon" ];
+  };
   settings.global.excludes = [
     ".direnv/"
     ".env"
