9692617fba14fb16f2963eb1fff640264cb3f076
John Marsden <john@johnmarsden.dev>
2025-01-14 13:05:47 -0500

    Add update script for talon repos

--- users/john/ergo/talon.nix
+++ users/john/ergo/talon.nix
@@ -1,52 +1,25 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
+let 
+  genTarget = target:
+  { 
+    name=".talon/user/${target.repo}"; 
+    value = {
+      source = pkgs.fetchFromGitHub {
+        owner = repo.owner;
+        repo = repo.repo;
+        rev = repo.rev;
+        hash = repo.hash;
+      };
+      recursive = true;
+    };
+  };
+  talon_repos = (lib.importToml ./talon_repos.toml).talon_repos;
 
+in
 {
-  home.file = {
-    ".talon/user/community" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "talonhub";
-        repo = "community";
-        rev = "bb240ba1cf958bd3e2c7791e5a57d09d205adc81";
-        hash = "sha256-Nnpm2d/2BQruT40fx2hzVvGkeDda8pPgx84rclHoOsA=";
-      };
-      recursive = true;
-    };
-    ".talon/user/rango" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "david-tejada";
-        repo = "rango-talon";
-        rev = "609641f8fe99fafe7e6f69441f60792fb7c51b60";
-        hash = "sha256-cyJkRZrOn2zrdi+roSOKDeLJjd/Bxk+Ra4lWreGqq+4=";
-      };
-      recursive = true;
-    };
-    ".talon/user/cursorless" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "cursorless-dev";
-        repo = "cursorless-talon";
-        rev = "0aba9e01299637655c7ca11cba7c99b965a1a488";
-        hash = "sha256-aAr4EVE3E3KjGgHcw/ruYmfi3wXKLhPYhHvOC2eQDPE=";
-      };
-      recursive = true;
-    };
-    ".talon/user/talon_hud" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "chaosparrot";
-        repo = "talon_hud";
-        rev = "0d676565b95f34841d7268b3667b4b780e50cfaa";
-        hash = "sha256-/uUZkh0SH/rh4jkNbTnZAJplZWHexRXQhVldwI0Gl7k=";
-      };
-      recursive = true;
-    };
-    ".talon/user/speak-the-spire-talon" = {
-      source = pkgs.fetchFromGitHub {
-        owner = "ProfDoof";
-        repo = "speak-the-spire-talon";
-        rev = "fd8a2cc9af6d968bf6134994fcef8b3f9df4f40b";
-        hash = "sha256-K6LcQKH7Js1umnweiD/tbulfk9m1WrfAp+UHIVIvCns=";
-      };
-      recursive = true;
-    };
+  
+
+  home.file = lib.listToAttrs (lib.map genTarget talon_repos) // {
     ".talon/user/gamemode" = {
       source = ./gamemode;
       recursive = true;
---
+++ users/john/ergo/update.zsh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+get_rev() {
+  git ls-remote $1 HEAD | cut -f 1
+}
+
+update() {
+    echo "[[talon_repos]]"
+    echo "owner=${1}";
+    echo "repo=${2}";
+    echo "rev="$(get_rev "git@github.com:${1}/${2}");
+    echo "hash="
+    echo
+}
+
+REPOS=(
+    'talonhub' 'community'
+    'david-tejada' 'rango-talon'
+    'cursorless-dev' 'cursorless-talon'
+    'chaosparrot' 'talon_hud'
+    'ProfDoof' 'speak-the-spire-talon'
+)
+
+
+
+for owner repo in "$REPOS[@]"
+do
+    update $owner $repo
+done > talon_repos.toml
