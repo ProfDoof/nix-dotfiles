dc4978f376c7b8442cc12a18699c17195c1de9c3
John Marsden <john@johnmarsden.dev>
2025-01-14 13:37:33 -0500

    Update hash generator

--- users/john/ergo/update.zsh
+++ users/john/ergo/update.zsh
@@ -8,7 +8,8 @@ update() {
     echo "owner = \"${1}\"";
     echo "repo = \"${2}\"";
     echo "rev = \""$(get_rev "git@github.com:${1}/${2}")"\"";
-    echo "hash = \"sha256-${1}${2}\""
+    combined="${1}${2}"
+    echo "hash = \"sha256-${(l:44::A:)combined}\""
     echo
 }
 
