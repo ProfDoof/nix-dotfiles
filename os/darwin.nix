8f13e21dba39ae570c497bf9e5dedfe957c81fd3
John Marsden <john@johnmarsden.dev>
2025-02-17 13:37:40 -0500

    Update private

--- os/darwin.nix
+++ os/darwin.nix
@@ -1,6 +1,6 @@
 { pkgs, ... }:
 {
-  services.nix-daemon.enable = true;
+  # services.nix-daemon.enable = true;
   nix.package = pkgs.nix;
   nixpkgs.hostPlatform = "x86_64-darwin";
   system.stateVersion = 5;
